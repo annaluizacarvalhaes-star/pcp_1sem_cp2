ec624878ce964aa94b92e9abfc161b972782bc0d
annaluizacarvalhaes-star <annaluizacarvalhaes@gmail.com>
2026-04-14 11:04:34 -0300

    exercicios

---
+++ pcp_1sem_cp2_ex01.py
@@ -0,0 +1,7 @@
+estado = input(print("Digite o número do estado de origem da carga: "))
+peso = input(print("Digite o peso da carga em toneladas: "))
+codigo = input(print("Digite o código da carga (número de 10 a 40: "))
+
+peso_kg = peso * 1000
+print(f'O peso da carga em kg é {peso_kg}')
+
