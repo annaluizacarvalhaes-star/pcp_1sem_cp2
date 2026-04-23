59724946d635c1a9554dbd746c5bfbd7bb4275aa
annaluizacarvalhaes-star <annaluizacarvalhaes@gmail.com>
2026-04-23 20:39:21 -0300

    exercicío 4

--- pcp_1sem_cp2_ex04.py
+++ pcp_1sem_cp2_ex04.py
@@ -1,57 +1,47 @@
 nome_funcionário = input("digite seu nome: ")
 
-cargos = {
-        "1": "Gerente",
-        "2": "Analista",
-        "3": "Assistente",
-        "4": "Estagiário"
-    }
-print("\n selecione seu cargo:")
-print(" 1 - gerente")
-print("2 - analista")
-print("3 - assistente")
-print("4 - estagiário")
-cargo= input(print("selecione seu cargo: "))
+print("Selecione seu cargo: ")
+print(" 1 - gerente ")
+print("2 - analista ")
+print("3 - assistente ")
+print("4 - estagiário ")
 
-salário_base= float(input(print(" Digite seu salario base:")))
-horas_extras= input(print(" digite suas horas extras trabalhadas: "))
-faltas_mes= input(print(" Digite suas faltas no mês: "))
-bonus= input(print("Recebeu bonus esse mês? (sim/não): "))
-if bonus == "sim":
-  valor_bonus= input(print(" Digite o valor recebido: "))
+
+cargo_input = int(input("Digite o número correspondente ao seu cargo: "))
+bonus1 = 0
+
+salário = float(input("Digite o seu salário: "))
+horas_extras = int(input("Digite a quantidade de horas extras trabalhadas: "))
+faltas = int(input("Digite a quantidade de faltas: "))
+bonus_resposta = input("Você recebeu bônus? (sim/não): ").lower()
+
+if bonus_resposta == "sim":
+  if cargo_input == 1:
+      bonus1 = 1000
+      print("Seu bônus é de R$1000,00 \n")
+  elif cargo_input == 2:
+     bonus1 = 500
+     print("Seu bônus é de R$500,00 \n")
+  elif cargo_input == 3:
+     bonus1 = 300
+     print("Seu bônus é de R$300,00 \n")
+  else:
+     bonus1 = 100
+     print("Seu bônus é de R$100,00 \n")
 else:
-  print(" continuar ")
+  print("Você não recebeu bônus.\n")
 
-  valor_hora_extra = float(horas_extras) * (salário_base * 0.15)
-  print(f" o valor ganho de horas extras: {valor_hora_extra}")
+def calculo_hora_extra (horas_extras, salário):
+  return horas_extras * (0.015 * salário)
 
-  desconto_falta = float(faltas_mes) * (salário_base * 0.2)
-  print(f" o valor descontado por faltas no mes foi: {desconto_falta}")
+def calculo_faltas (faltas, salário):
+  return faltas * (0.02 * salário)
 
-  cargos_bonus = {
-      "1": ["Gerente", 1000.00],
-      "2": ["Analista", 500.00],
-      "3": ["Assistente", 300.00],
-      "4": ["Estagiário", 100.00]
+print(f"Seu salário bruto é de: R$ {salário}")
+print("Seu total de acrescimos é de:", calculo_hora_extra(horas_extras, salário))
+print("Seu total de descontos é de:", calculo_faltas(faltas, salário))
 
-  }
+# Calculo do salário total
+salario_total = salário + bonus1 + calculo_hora_extra(horas_extras, salário) - calculo_faltas(faltas, salário)
 
-if cargo == 1:
-  bonus1 = 1000
-elif cargo == 2:
-  bonus1 = 500
-elif cargo == 3:
- bonus1 = 300
-else:
-  bonus1 = 100
-
-print(f" o valor do salario bruto é: {salário_base}")
-
-total_acrescimo = float(valor_hora_extra + bonus1)
-print(f" o valor total de acrecimos de bonus no salario total é : {total_acrescimo}")
-
-total_descontos= float(salário_base) - (desconto_falta)
-print(f" o valor descontado por faltas do salário bruto foi: {total_descontos}")
-
-salario_final = float (total_acrescimo) + (total_descontos)
-print(f" o valor do salário recebido no final do mes foi: {salario_final}")
+print("Seu salário total é de: R$", salario_total)
